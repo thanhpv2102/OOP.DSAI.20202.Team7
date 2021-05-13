5734d33a81f3af5ea8832be535a2d45d7bb0ce94
mrcuongtroll <38219186+mrcuongtroll@users.noreply.github.com>
2021-05-13 20:38:32 +0700

    added forces related classes

---
+++ ForceRepresentation/src/force/FrictionalForce.java
@@ -0,0 +1,14 @@
+package force;
+
+public class FrictionalForce extends HorizontalForce{
+	//Base direction: Left
+	
+	public FrictionalForce(double rootX, double rootY) {
+		super(rootX, rootY);
+	}
+	public FrictionalForce(double rootX, double rootY, double magnitude) {
+		super(rootX, rootY, magnitude);
+		// TODO Auto-generated constructor stub
+	}
+
+}
