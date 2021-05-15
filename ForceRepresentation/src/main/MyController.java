b6ebd0226671adf1b0c510b2de4248b0e1886de9
theRealLasHiD <thanh.pv.ds@gmail.com>
2021-05-15 11:48:14 +0700

    add choose object + add cyclinder rotation

--- ForceRepresentation/src/main/MyController.java
+++ ForceRepresentation/src/main/MyController.java
@@ -8,6 +8,7 @@ import java.util.ResourceBundle;
 import javafx.animation.Animation;
 import javafx.animation.Interpolator;
 import javafx.animation.ParallelTransition;
+import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -15,18 +16,27 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 public class MyController implements Initializable {
 
 	private int BACKGROUND_WIDTH = 1653;
 	private static ParallelTransition parallelTransition;
-	private static TranslateTransition translateTransition;
-	private static TranslateTransition translateTransition2;
-	
+	private static RotateTransition rotateTransition1;
+	private static RotateTransition rotateTransition2;
+	private static TranslateTransition bg_trans1;
+	private static TranslateTransition bg_trans2;
+
 	@FXML
 	private Slider forceSlider;
 
@@ -36,31 +46,103 @@ public class MyController implements Initializable {
 	@FXML
 	private ImageView bg2;
 
+	@FXML
+	private Rectangle bg_cube;
+
+	@FXML
+	private Circle bg_cyclinder;
+
+	@FXML
+	private RadioButton radio_cube;
+
+	@FXML
+	private RadioButton radio_cyclinder;
+
+	ToggleGroup group = new ToggleGroup();
+
+	@FXML
+	private TextField input_mass;
+
+	@FXML
+	private TextField input_radius;
+
+	@FXML
+	private TextField input_sizeLength;
+
+	@FXML
+	private Text txt_radius;
+
+	@FXML
+	private Text txt_sizeLength;
+
+	@FXML
+	private Line horizontal_line;
+
+	@FXML
+	private Line vertical_line;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
-		translateTransition = new TranslateTransition(Duration.millis(10000), bg1);
-		translateTransition.setFromX(0);
-		translateTransition.setToX(-1 * BACKGROUND_WIDTH);
-		translateTransition.setInterpolator(Interpolator.LINEAR);
-		
-		translateTransition2 = new TranslateTransition(Duration.millis(10000), bg2);
-		translateTransition2.setFromX(0);
-		translateTransition2.setToX(-1 * BACKGROUND_WIDTH);
-		translateTransition2.setInterpolator(Interpolator.LINEAR);
+		radio_cube.setToggleGroup(group);
+		radio_cyclinder.setToggleGroup(group);
 
-		parallelTransition = new ParallelTransition(translateTransition, translateTransition2);
-		parallelTransition.setCycleCount(Animation.INDEFINITE);
+		group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+			@Override
+			public void changed(ObservableValue<? extends Toggle> arg0, Toggle arg1, Toggle arg2) {
+				RadioButton button = (RadioButton) group.getSelectedToggle();
+				if (button.getText().equals("Cube")) {
+					input_radius.setVisible(false);
+					input_sizeLength.setVisible(true);
+					txt_radius.setVisible(false);
+					txt_sizeLength.setVisible(true);
+					bg_cyclinder.setVisible(false);
+					bg_cube.setVisible(true);
+					horizontal_line.setVisible(false);
+					vertical_line.setVisible(false);
+				} else if (button.getText().equals("Cyclinder")) {
+					input_radius.setVisible(true);
+					input_sizeLength.setVisible(false);
+					txt_radius.setVisible(true);
+					txt_sizeLength.setVisible(false);
+					bg_cyclinder.setVisible(true);
+					bg_cube.setVisible(false);
+					horizontal_line.setVisible(true);
+					vertical_line.setVisible(true);
+				}
+			}
+		});
+
+		bg_trans1 = new TranslateTransition(Duration.millis(10000), bg1);
+		bg_trans1.setFromX(0);
+		bg_trans1.setToX(-1 * BACKGROUND_WIDTH);
+		bg_trans1.setInterpolator(Interpolator.LINEAR);
+
+		bg_trans2 = new TranslateTransition(Duration.millis(10000), bg2);
+		bg_trans2.setFromX(0);
+		bg_trans2.setToX(-1 * BACKGROUND_WIDTH);
+		bg_trans2.setInterpolator(Interpolator.LINEAR);
+
+		rotateTransition1 = new RotateTransition(Duration.millis(10000), horizontal_line); 
+		rotateTransition1.setByAngle(720);
+		rotateTransition2 = new RotateTransition(Duration.millis(10000), vertical_line); 
+		rotateTransition2.setByAngle(720);
 		
+		rotateTransition1.setInterpolator(Interpolator.LINEAR);
+		rotateTransition2.setInterpolator(Interpolator.LINEAR);
+
+		parallelTransition = new ParallelTransition(bg_trans1, bg_trans2, rotateTransition1, rotateTransition2);
+		parallelTransition.setCycleCount(Animation.INDEFINITE);
+
 		forceSlider.valueProperty().addListener(new ChangeListener<Number>() {
 			@Override
 			public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
-				setBackgroundTransitionRate(Math.abs(arg2.floatValue()));
+				setBackgroundTransitionRate(Math.abs(arg2.doubleValue()));
 			}
 		});
 	}
-	
-	public static void setBackgroundTransitionRate(float speed) {	
+
+	public static void setBackgroundTransitionRate(double speed) {	
 		//slowly speed up animation from old speed to new speed
 		float i = (float) parallelTransition.getRate();
 		while (i <= speed) {
