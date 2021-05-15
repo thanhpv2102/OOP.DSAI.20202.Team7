e1175b81612d72785ce002d740aa289034a47e04
mrcuongtroll <38219186+mrcuongtroll@users.noreply.github.com>
2021-05-15 12:35:19 +0700

    added functionalities for friction sliders

--- ForceRepresentation/src/main/MyController.java
+++ ForceRepresentation/src/main/MyController.java
@@ -1,28 +1,72 @@
 package main;
+
+import force.ActorForce;
+import objects.*;
+
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
- 
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
  
 public class MyController implements Initializable {
+	private ActorForce force = new ActorForce(0.0, 0.0);
+	private Surface surface = new Surface(0.25, 0.25);
+	
  
    @FXML
    private Button myButton;
   
    @FXML
    private TextField myTextField;
+   
+   @FXML
+   private Slider staticSlider;
+   
+   @FXML
+   private Slider kineticSlider;
   
    @Override
    public void initialize(URL location, ResourceBundle resources) {
  
-       // TODO (don't really need to do anything here).
+	   
+       //Alter static friction coef with staticSlider
+	   staticSlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+		@Override
+		public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+			if (staticSlider.getValue() >= kineticSlider.getValue()) {
+				staticSlider.adjustValue(kineticSlider.getValue());
+			}
+			surface.setStaticFrictionCoef((double)staticSlider.getValue());
+//			System.out.println("static:" + surface.getStaticFrictionCoef());
+		}
+	   });
+	   
+	   
+	   //Alter kinetic friction coef with kineticSlider
+	   kineticSlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+		@Override
+		public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+			// TODO Auto-generated method stub
+			if (kineticSlider.getValue() <= staticSlider.getValue()) {
+				kineticSlider.adjustValue(staticSlider.getValue());
+			}
+			surface.setKineticFrictionCoef((double)kineticSlider.getValue());
+//			System.out.println("kinetic:" + surface.getKineticFrictionCoef());
+		}
+		   
+	   });
       
    }
  
